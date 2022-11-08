a463afca13a1a72c0a9f1f3376570b578e011d0a
Tormod Sletteboe <tormod.slettebo@gmail.com>
2022-11-08 15:12:06 -0600

    autentication stuff

--- server/routes/pets.router.js
+++ server/routes/pets.router.js
@@ -1,29 +1,42 @@
 const express = require('express');
 const router = express.Router();
 const pool = require('../modules/pool');
-
+const {rejectUnauthenticated} = require('../modules/authentication-middleware');
 // This route *should* return the logged in users pets
-router.get('/', (req, res) => {
-    console.log('/pet GET route');
-    console.log('is authenticated?', req.isAuthenticated());
-    console.log('user', req.user);
-    let queryText = `SELECT * FROM "pets"`;
-    pool.query(queryText).then((result) => {
-        res.send(result.rows);
-    }).catch((error) => {
-        console.log(error);
-        res.sendStatus(500);
-    });
+router.get('/',rejectUnauthenticated, (req, res) => {
+    
+        console.log('/pet GET route');
+        console.log('is authenticated?', req.isAuthenticated());
+        console.log('user', req.user);
+        let queryText = `SELECT * FROM "pets" WHERE "user_id"=$1`;
+        pool.query(queryText, [req.user.id]).then((result) => {
+            res.send(result.rows);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+   
+
 });
 
 // This route *should* add a pet for the logged in user
-router.post('/', (req, res) => {
-    console.log('/pet POST route');
-    console.log(req.body);
-    console.log('is authenticated?', req.isAuthenticated());
-    console.log('user', req.user);
-    res.sendStatus(200);
-    
+router.post('/',rejectUnauthenticated, (req, res) => {
+   
+        console.log('/pet POST route');
+        console.log(req.body);
+        console.log('is authenticated?', req.isAuthenticated());
+        console.log('user', req.user);
+        let queryText = `INSERT INTO "pets" ("name","user_id")
+                         VALUES($1,$2)
+                         ;`;
+        pool.query(queryText, [req.body.name,req.user.id]).then((result) => {
+            res.sendStatus(201);
+        }).catch((error) => {
+            console.log(error);
+            res.sendStatus(500);
+        });
+        
+   
 });
 
 module.exports = router;
